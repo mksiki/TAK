a9ecbbccbd4626055751c2404658593ad9da106a
Jesus Antonio Munoz <antonio@Jesuss-MBP.attlocal.net>
2024-01-16 13:43:02 -0800

    Added Clerk API Auth

---
+++ tak/app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <p className='text-3xl font-bold text-indigo-500'>TAK</p>
+      <Button>Click Me</Button>
+    </div>
+    
+  )
+}
